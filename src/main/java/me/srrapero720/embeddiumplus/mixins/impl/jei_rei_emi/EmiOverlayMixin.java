0ffc3c05b4f0a70c684ae5fd4e936fcc1076d6ed
J-RAP <SrRapero720@hotmail.com>
2024-05-13 15:40:56 -0600

    some fixes around buildscript

--- src/main/java/me/srrapero720/embeddiumplus/mixins/impl/jei_rei_emi/EmiOverlayMixin.java
+++ src/main/java/me/srrapero720/embeddiumplus/mixins/impl/jei_rei_emi/EmiOverlayMixin.java
@@ -12,11 +12,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Iterator;
-import java.util.List;
 
 @Mixin(value = EmiScreenManager.class, remap = false)
 @Pseudo
